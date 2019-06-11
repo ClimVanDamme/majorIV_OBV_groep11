90c0d7b6dbfdc2837949338c5cfdf296cd8d22cd
Bavo Vanderghote <bavo.vanderghote@gmail.com>
2019-06-11 23:56:33 +0200

    seeder aanpassen

--- buddy app/server/seeder.js
+++ buddy app/server/seeder.js
@@ -18,24 +18,26 @@ const data = [
 const shows = [
   {
     title: "Macbeth",
-    description: "Lang leve de koning",
-    date: "2019-07-16 16:00:00",
+    description:
+      "Macbeth van Shakespeare is ongetwijfeld het meest demonische werk van Giuseppe Verdi. De focus ligt op de vergankelijkheid van de macht. We zijn getuige hoe er een ketting van geweld, terreur en paranoia ontstaat waarbij Macbeth, Lady Macbeth en de heksen op een fatale en destructieve manier aan elkaar zijn overgeleverd.",
+    date: "2019-07-06 19:30:00",
     category: "Opera",
     length: "180"
   },
   {
-    title: "Het Zwanenmeer",
-    description: "Dans, dans, dans...",
-    date: "2019-08-05 20:00:00",
+    title: "Bach Studies",
+    description:
+      "Ballet Vlaanderen haalt met de Franse choreograaf Benjamin Millepied een van de meest gerenommeerde hedendaagse balletchoreografen voor het eerst naar België. Hij creëert voor het eerst een avondvullend werk gebaseerd op werk van Johann Sebastian Bach. Het belooft een unieke, abstracte en toch hoogst emotionele balletavond te worden, gecreëerd door een van de meest innovatieve balletchoreografen van het moment.",
+    date: "2019-07-19 20:00:00",
     category: "Ballet",
-    length: "120"
+    length: "105"
   }
 ].map(item => ({ ...item, _id: mongoose.Types.ObjectId() }));
 
 const characters = [
   {
     name: "Macbeth",
-    traits: ["Schots", "Generaal", "moedig"],
+    traits: ["Man", "Tiran", "Lady Macbeth"],
     personality: "Macbeth heeft veel ambitie en veel moed.",
     story:
       "Mactbeth krijgt een voorspelling van drie heksen dat hij op een dag koning van Schotland zal zijn. Hij heeft koning Duncan vermoord.",
@@ -45,7 +47,7 @@ const characters = [
   },
   {
     name: "Koning Duncan",
-    traits: ["Schots", "Koning", "2 zonen"],
+    traits: ["Man", "Koning", "2 zonen"],
     personality: "Koning Duncan is een genereuze, vriendelijke vader.",
     story:
       "Koning Duncan wordt vermoord door zijn de generaal die hij volledig vertrouwde, Macbeth.",
@@ -55,7 +57,7 @@ const characters = [
   },
   {
     name: "Lady Macbeth",
-    traits: ["Schots", "Vrouw van Macbeth", "Zelfmoord"],
+    traits: ["Vrouw", "Sluw", "Macbeth"],
     personality:
       "Lady Macbeth is egoïstisch, maar er zit toch wat goeds diep vanbinnen.",
     story:
@@ -65,13 +67,32 @@ const characters = [
     show_id: shows[0]._id
   },
   {
-    name: "Prins Siegfried",
-    traits: ["Pas meerderjarig", "Verliefd op Odette", "Held"],
-    personality: "Siegfried is jong en verliefd.",
+    name: "Macduff",
+    traits: ["Man", "Rechtvaardig", "Warm gezin"],
+    personality: "Macduff strijdt voor rechtvaardigheid.",
     story:
-      "Siegfried is pas meerderjarig en moet trouwen van zijn moeder. Hij wordt verliefd op Odette.",
-    summary: ["Pas meerderjarig", "Verliefd op Odette", "Held"],
-    quote: "Ik zoek mijn ware liefde.",
+      "Macduff gaat naar Engeland om een vriend te helpen. Macbeth komt daar achter en laat het achtergebleven gezin van Macduff uitmoorden. Macduff is degene die Macbeth dood.",
+    summary: ["Rechtvaardig", "Gezin uitgemoord", "Held"],
+    quote: "Voor mijn gezin moet je door mij.",
+    show_id: shows[0]._id
+  },
+  {
+    name: "Banquo",
+    traits: ["Man", "Geest", "Zoon Fleance"],
+    personality: "Banquo komt terug als een geest.",
+    story:
+      "Banquo is een veldheer van koning Duncan, hij is er bij wanneer Macbeth de voorspelling van de heksen ontvangt. Zij voorspellen dat hij koningen gaat voortbrengen. Macbeth ziet dit als een dreigement voor zijn koningschap en besluit Banquo te vermoorden.",
+    summary: ["Geest", "Zoon wordt koning", "Veldheer"],
+    quote: "Van mij raak je nog niet zo makkelijk af.",
+    show_id: shows[0]._id
+  },
+  {
+    name: "Danser 1",
+    traits: ["Man", "Rechtvaardig", "Warm gezin"],
+    personality: "Danser 1 is goed",
+    story: "Danser 1 is de beste",
+    summary: ["Rechtvaardig", "Gezin uitgemoord", "Held"],
+    quote: "Dans met mij.",
     show_id: shows[1]._id
   }
 ].map(item => ({ ...item, _id: mongoose.Types.ObjectId() }));
